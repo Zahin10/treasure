887672dc4d939f7418d8f009aa39ed28bfdcd010
Zahin Chowdhury <78267979+Zahin10@users.noreply.github.com>
2024-08-04 18:58:24 -0400

    Delete src/pages/app.js

--- src/pages/app.js
+++
@@ -1,9 +0,0 @@
-import { MantineProvider } from '@mantine/core';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Component {...pageProps} />
-    </MantineProvider>
-  );
-}
